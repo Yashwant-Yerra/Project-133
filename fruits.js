185d1193bd8e84cbc36c6d3e013b3e255f1f6982
Yashwant-Yerra <86163402+Yashwant-Yerra@users.noreply.github.com>
2021-12-01 17:58:02 -0600

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,42 @@
+img = "" ;
+objects = [] ;
+
+function preload() {
+    img = loadImage('fruits.png') ;
+}
+
+function setup() {
+    canvas = createCanvas(750,450) ;
+    canvas.position(390, 350) ;
+
+    detector = ml5.objectDetector('cocossd' , modelloaded) ;
+    document.getElementById("status").innerHTML = "Status: Detecting objects" ;
+}
+function modelloaded() {
+    console.log("cocossd is initialised.") ; 
+    detector.detect(img, gotResult) ;
+}
+function gotResult(error, results) {
+    if(error) {
+        console.log(error) ;
+    }
+    else {
+        console.log(results) ;
+        objects =  results ;
+    }
+}
+function draw() {
+    image(img, 0, 0, 750, 450) ;
+
+    for(i=0 ; i<objects.length ; i++) {
+        document.getElementById("status").innerHTML = "Status: Detected objects." + "<br>" + "There are mainly 10 fruits in this picture. Cocossd detected 4 of them." ;
+        accuracyPercent =floor(objects[i].confidence *100);
+        fill('#000') ;
+        text(objects[i].label + " : " + accuracyPercent + "% accuracy" , objects[i].x + 17, objects[i].y + 30) ;
+        textSize(18) ;
+        textFont('Georgia');
+        noFill() ;
+        stroke('#000') ;
+        rect(objects[i].x,objects[i].y, objects[i].width, objects[i].height) ;
+    }
+}
